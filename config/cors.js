82d5585e59ea59b1824911c0458408c38db2dc3c
Nhan Quốc Việt <qviet092@gmail.com>
2024-09-17 23:30:12 +0700

    fix cors front-end

--- config/cors.js
+++ config/cors.js
@@ -1,12 +1,20 @@
 require('dotenv').config()
 
+const allowedOrigins = [
+    'https://phosocial-frontend.vercel.app',
+    'http://localhost:3000',
+];
+
 const configCors = (app) => {
     app.use(function (req, res, next) {
-        res.setHeader('Access-Control-Allow-Origin', 'https://phosocial-frontend.vercel.app/' ||'http://localhost:3000');
+        const origin = req.headers.origin;
+        if (allowedOrigins.includes(origin)) {
+            res.setHeader('Access-Control-Allow-Origin', origin);
+        }
         res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
         res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
         res.setHeader('Access-Control-Allow-Credentials', true);
-    
+
         next();
     });
 }
