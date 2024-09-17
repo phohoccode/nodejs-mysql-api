c324f624a2adcb0f16b98781a84f104582a433c5
Nhan Quốc Việt <qviet092@gmail.com>
2024-09-17 23:18:52 +0700

    test

--- config/cors.js
+++ config/cors.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 const configCors = (app) => {
     app.use(function (req, res, next) {
-        res.setHeader('Access-Control-Allow-Origin', process.env.REACT_APP_URL ||'http://localhost:3000');
+        res.setHeader('Access-Control-Allow-Origin', 'https://phosocial-frontend.vercel.app/' ||'http://localhost:3000');
         res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
         res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
         res.setHeader('Access-Control-Allow-Credentials', true);
